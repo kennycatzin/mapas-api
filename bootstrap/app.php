88a499f347af2c4357745e7cde7ddd2f901cfcdb
Kenny Catzin Ruiz <kenn2506@gmail.com>
2020-12-02 18:50:15 -0600

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -96,10 +96,10 @@ $app->routeMiddleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+
 // $app->register(App\Providers\EventServiceProvider::class);
 
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
-
 
 /*
 |--------------------------------------------------------------------------
